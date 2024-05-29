d9e2d0bb289eb099ceaf705729fba8b074439ed4
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-05-29 21:37:32 +0530

    Initial Commit

---
+++ HospitalManagementSystem/HospitalManagementSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  HospitalManagementSystemApp.swift
+//  HospitalManagementSystem
+//
+//  Created by Akshay Jha on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HospitalManagementSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
